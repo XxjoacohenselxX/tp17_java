ed35b0afd41d8437c51686278c79f23b2d2f6398
Jose Fuentes <webmaster@inforock.com>
2024-09-14 10:31:11 -0300

    Parámetros de conexión a la base de datos corregidos

--- evaluaciones-app/src/main/java/evaluacion/DatabaseConnector.java
+++ evaluaciones-app/src/main/java/evaluacion/DatabaseConnector.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnector {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/evaluaciones_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/evaluaciones";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
